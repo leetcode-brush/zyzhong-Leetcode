884e6baeba084076e86023cd58e4cd23af653a74
zhearing <zzy19950621@gmail.com>
2019-02-27 20:52:23 +0800

    Update main.cpp

--- Stack & Queue/0347-Top-K-Frequent-Elements/main.cpp
+++ Stack & Queue/0347-Top-K-Frequent-Elements/main.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int, int> record;
         for (int i = 0; i < nums.size(); i++)
-            record[nums[i]] ++;
+            record[nums[i]]++;
         
         vector<pair<int, int>> vec;
         for (auto iter = record.begin(); iter != record.end(); ++iter)
